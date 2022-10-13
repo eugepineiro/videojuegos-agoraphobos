c25b99afcf951bddedf67db239bab46b25b36ff1
Luciano Boccardi <luciano.boccardi@mudafy.com>
2022-10-13 00:43:57 -0300

    added Door that rotates when opening

--- Assets/Scripts/Entities/Door.cs
+++ Assets/Scripts/Entities/Door.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 { 
+    IEnumerator Rotate(Transform transform, Vector3 rotationVector) {
+        Debug.Log("Called coroutine");
+        Quaternion fromAngle = transform.rotation;
+        Quaternion toAngle = Quaternion.Euler(rotationVector);
+
+         for (float t = 0f; t <= 1; t += Time.deltaTime) {
+            transform.rotation = Quaternion.Slerp(fromAngle, toAngle, t);
+            yield return null;
+         }
+    }
+    
     private void OnTriggerEnter(Collider other)
-    { 
-        Destroy(this.gameObject);
+    {
+        Debug.Log("Entered collision");
+        if ( other.CompareTag("Player") ) 
+            StartCoroutine(Rotate(transform, new Vector3(0, 90f, 0)));
+            GetComponent<Collider>().enabled = false;
         EventsManager.instance.EventDoorOpened(); //Opened door sound
     }
 }
