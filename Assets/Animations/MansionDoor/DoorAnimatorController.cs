90af8b0754843e898b249380ddd46edb43641901
eugepineiro <epineiro@itba.edu.ar>
2022-11-12 16:30:49 -0300

    Add hall controller and fix statics

--- Assets/Animations/MansionDoor/DoorAnimatorController.cs
+++ Assets/Animations/MansionDoor/DoorAnimatorController.cs
@@ -7,17 +7,22 @@ public class DoorAnimatorController : MonoBehaviour
     private Animator _animator; 
     private const bool DOOR_PLAY_OPEN_ANIMATION = true;
     private const bool DOOR_PLAY_CLOSE_ANIMATION = false;
+    private HallPuzzleController _hallPuzzleController;
+
     void Start()
     {
         _animator = GetComponent<Animator>();
+  		_hallPuzzleController = GameObject.Find("HallPuzzles").GetComponent<HallPuzzleController>(); 
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if ( other.CompareTag("Player") )
+        if ( other.gameObject.name == "MansionKey" /*other.CompareTag("Player")*/ )
         {
+			Debug.Log("Key hit mansion door");
             SetAnimatorParams(DOOR_PLAY_OPEN_ANIMATION);
             EventsManager.instance.EventDoorOpened(); //Opened door sound
+ 			_hallPuzzleController.OpenMansionDoor();
         }
     }
     
