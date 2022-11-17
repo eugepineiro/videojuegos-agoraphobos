0ad1c9540dffc3b46ccc5cab3fd1e0fc47e1f681
Luciano Boccardi <luciano.boccardi@mudafy.com>
2022-11-16 21:25:33 -0300

    fixed Door Slerp

--- Assets/Scripts/Entities/Door.cs
+++ Assets/Scripts/Entities/Door.cs
@@ -6,11 +6,11 @@ public class Door : MonoBehaviour
 { 
     IEnumerator Rotate(Transform transform, Vector3 rotationVector) {
         Debug.Log("Called coroutine");
-        Quaternion fromAngle = transform.rotation;
+        Quaternion fromAngle = transform.localRotation;
         Quaternion toAngle = Quaternion.Euler(rotationVector);
 
          for (float t = 0f; t <= 1; t += Time.deltaTime) {
-            transform.rotation = Quaternion.Slerp(fromAngle, toAngle, t);
+            transform.localRotation = Quaternion.Slerp(fromAngle, toAngle, t);
             yield return null;
          }
     }
@@ -20,6 +20,7 @@ public class Door : MonoBehaviour
         Debug.Log("Entered collision");
         if ( other.CompareTag("Player") ) {
             Vector3 localRotation = transform.localEulerAngles;
+            Debug.Log($"local rotation is {localRotation.y}");
             StartCoroutine(Rotate(transform, new Vector3(localRotation.x, localRotation.y + 90f, localRotation.z)));
             GetComponent<Collider>().enabled = false;
         } 
