10d4691c1989ee80101cc9d9f2b50196454a44ed
eugepineiro <epineiro@itba.edu.ar>
2022-12-01 15:37:12 -0300

    Add chapter management and update UI

--- Assets/Scripts/UI/UIButtonLogic.cs
+++ Assets/Scripts/UI/UIButtonLogic.cs
@@ -5,12 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class UIButtonLogic : MonoBehaviour
 {
-
     public void LoadMenuScene() => SceneManager.LoadScene("MenuScene");
 
-    public void LoadLevelScene() => SceneManager.LoadScene("LoadbarScene");
+    public void LoadLevelScene()
+    {
+        GlobalData.instance.SetChapter("SampleScene");
+        SceneManager.LoadScene("LoadbarScene");
+    }
 
     public void LoadInfoScene() => SceneManager.LoadScene("InformationScene"); 
+    
+    public void LoadChapterScene() => SceneManager.LoadScene("ChapterScene");
+
+    public void LoadLaberynthScene()
+    {
+        Debug.Log($"btn labery");
+        GlobalData.instance.SetChapter("Laberynth");
+        SceneManager.LoadScene("LoadbarScene");
+
+        //SceneManager.LoadScene("Laberynth"); 
+    }
 
     public void CloseGame() => Application.Quit();
     
